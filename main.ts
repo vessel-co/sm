d1446bfd3025853d454d28fc65bf803f1936148f
Nebez Briefkani <me@nebezb.com>
2023-09-11 14:34:58 -0400

    v0.0.5

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ import { fromSso, TokenError } from "./src/ssoTokenProvider.ts";
 
 const command = new Command()
   .name("sm")
-  .version("0.0.4")
+  .version("0.0.5")
   .description("A single command line tool for SSM'ing into ec2 instances");
 
 const promptForSsoSession = async () => {
